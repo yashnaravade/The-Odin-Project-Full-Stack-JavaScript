7e1902f4e267a7bf3d34a300cf4d502d9dbf771f
Yash Naravade <yashdip123@gmail.com>
2023-07-21 20:28:39 +0530

    Getting json data with try catch

--- JavaScript/Promises.js
+++ JavaScript/Promises.js
@@ -8,14 +8,16 @@ new Promise(function (resolve, reject) {
       reject("Something went wrong!");
     }
   }, 1000);
-}).then(()=>{
-    console.log("Resolved")
-}).catch(()=>{
+})
+  .then(() => {
+    console.log("Resolved");
+  })
+  .catch(() => {
     console.log("Rejected");
-}).finally(()=>
-{
+  })
+  .finally(() => {
     console.log("We're done brazaa!!!");
-});
+  });
 
 // With Async/Await
 
@@ -42,3 +44,14 @@ async function login() {
 
 login();
 
+async function getUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("E :", error);
+  }
+} 
+
+getUsers();
